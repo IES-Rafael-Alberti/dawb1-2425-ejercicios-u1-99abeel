79e386a8a86b229bd058212aacba537473dde39e
99abeel <scdvabel@iesrafaelalberti.es>
2024-09-27 20:42:37 +0200

    Ejercicios 2 al 5

---
+++ src/ej04.py
@@ -0,0 +1,3 @@
+celsius = int(input ("Introduzca la temperatura el grados Celsius: "))
+Fah = (celsius * 9/5) + 32
+print ("La temperatura en grados Fahrenheit es", Fah)
---
+++ src/ej05.py
@@ -0,0 +1,7 @@
+precio = int(input ("Introduzca el precio sin IVA: "))
+
+iva = int(input ("Introduzca el IVA que desea aplicar: "))
+
+total = precio * iva
+
+print ("El precio del articulo con IVA es", total)
