0a766ebed051c9e79d7912fd38795be2726bc186
99abeel <scdvabel@iesrafaelalberti.es>
2024-10-18 21:11:12 +0200

    actt

--- src/ej32.py
+++ src/ej32.py
@@ -7,5 +7,32 @@ def sucesion (maximo: int) -> str:
     fin = primero + segundo
 
     while  fin <= maximo:
+        serie += " " + str(fin)
+
+        primero= segundo
+        segundo= fin
+        fin = primero + segundo
+
+    return serie
+
+def convertir_num(entrada: str) -> bool:
+    numero = None
+    try:
+        numero = int(entrada)
+    except ValueError:
+        print("*ERROR*")
+
+    return numero
+
+
+def __main__():
+    entrada = input("Dame un numero: ")
+    numero = convertir_num(entrada)
+
+    if numero is not None:
+        print(sucesion(int(entrada)))
+
+if __name__ == "__main__":
+    __main__()
         
 
